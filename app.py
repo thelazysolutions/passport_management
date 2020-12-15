0f1547de9cc3aca959ac1500520c621ce1378506
Dattaprasad Ekavade <dattaprasad@ajency.in>
2020-12-15 11:19:03 +0530

    app.secret key moved for heroku

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 import os
 
 app = Flask(__name__)
-
+app.secret_key = os.urandom(12)
 @app.route('/')
 def home():
     if not session.get('logged_in'):
@@ -25,5 +25,5 @@ def logout():
     return home()
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
+    
     app.run(debug=True)
